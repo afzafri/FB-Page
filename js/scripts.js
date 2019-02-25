507c6b4590a9cb0e03d7925fabf5013f48d63d6f
Afif Zafri <zafri_21596@yahoo.com>
2019-02-25 17:47:20 +0800

    include fb js sdk

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '2052443064804179',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v3.2'
+    });
+  };
+
+});
