01d08e52468299108491e147b446413e37f4817e
Afif Zafri <zafri_21596@yahoo.com>
2019-02-25 17:55:40 +0800

    added facebook login, with permission to manage and publish page

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,26 @@ $(document).ready(function() {
 
   window.fbAsyncInit = function() {
     FB.init({
-      appId            : '2052443064804179',
+      appId            : '799563926836864',
       autoLogAppEvents : true,
       xfbml            : true,
       version          : 'v3.2'
     });
   };
+
+  $('#loginBtn').click(function() {
+    fbLogin();
+  });
+
+  function fbLogin() {
+    FB.login(function(response) {
+        if (response.authResponse) {
+          getUser();
+          getPagesList();
+        } else {
+          $("#userInfo").append('Login Failed. User cancelled login or did not fully authorize.').hide().fadeIn('slow');
+        }
+    }, {scope: ['manage_pages', 'publish_pages']});
+  }
 
 });
